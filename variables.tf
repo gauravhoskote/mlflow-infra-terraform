9312cfc510404da5dcee5d240b2aac1ce329ba67
Gaurav Hoskote <gauravgourang.hoskote@phoenix.edu>
2025-09-07 18:22:48 -0500

    init

--- variables.tf
+++ variables.tf
@@ -4,8 +4,8 @@ variable "env"     { type = string }
 variable "region"  { type = string }
 
 variable "vpc_cidr" { default = "10.0.0.0/16" }
-variable "public_subnet_cidrs"  { default = ["10.0.1.0/24","10.0.2.0/24"] }
-variable "private_subnet_cidrs" {  default = ["10.0.11.0/24","10.0.12.0/24"] }
+variable "public_subnet_cidrs"  { default = ["10.0.50.0/24", "10.0.60.0/24"] }
+variable "private_subnet_cidrs" {  default = ["10.0.150.0/24", "10.0.160.0/24"] }
 
 variable "db_username"       {  default = "mlflow" }
 variable "db_password"       {  sensitive = true }
